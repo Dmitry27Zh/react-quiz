ef7146b25ae94b93a0a9298ef650911b55b809ff
Dmitry <dmitry27zh@gmail.com>
2023-12-28 19:41:46 +0500

    Add template

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+
+function Game() {
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width: '50%' }} className="progress__inner"></div>
+      </div>
+      <h1>Что такое useState?</h1>
+      <ul>
+        <li>Это функция для хранения данных компонента</li>
+        <li>Это глобальный стейт</li>
+        <li>Это когда на ты никому не нужен</li>
+      </ul>
+    </>
+  )
+}
+
+function Result() {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt="" />
+      <h2>Вы отгадали 3 ответа из 10</h2>
+      <button>Попробовать снова</button>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  )
+}
+
+export default App
